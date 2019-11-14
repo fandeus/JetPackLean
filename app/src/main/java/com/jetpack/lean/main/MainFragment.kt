0badcfd594167f8828afa6888d175d6e44845833
VenRen <fandeus.ren@outlook.com>
2019-11-14 11:01:20 +0800

    首页Fragment

--- app/src/main/java/com/jetpack/lean/main/MainFragment.kt
+++ app/src/main/java/com/jetpack/lean/main/MainFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -14,9 +13,7 @@ import com.jetpack.lean.R
 
 class MainFragment : Fragment() {
 
-
-    private val fragmentList = mutableListOf<Fragment>()
-    private lateinit var fragmentTransaction: FragmentTransaction
+    private lateinit var currentFragment: Fragment
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,26 +31,27 @@ class MainFragment : Fragment() {
 
         toolbar.title = "主页"
 
-        fragmentList.add(HomeFragment())
-        fragmentList.add(LikeFragment())
-        fragmentList.add(MineFragment())
+        currentFragment = Fragment()
 
-        val fragmentManager: FragmentManager = childFragmentManager
-        fragmentTransaction = fragmentManager.beginTransaction()
-
+        val homeFragment: Fragment = HomeFragment()
+        val likeFragment: Fragment = LikeFragment()
+        val mineFragment: Fragment = MineFragment()
 
         val navigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.main_home -> {
-                    selectedPosition(0)
+                    toolbar.title = "主页"
+                    selectedFragment(homeFragment)
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.main_market -> {
-                    selectedPosition(1)
+                    toolbar.title = "收藏"
+                    selectedFragment(likeFragment)
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.main_mine -> {
-                    selectedPosition(2)
+                    toolbar.title = "我的"
+                    selectedFragment(mineFragment)
                     return@OnNavigationItemSelectedListener true
                 }
             }
@@ -61,12 +59,22 @@ class MainFragment : Fragment() {
         }
 
         bottomNav.setOnNavigationItemSelectedListener(navigationItemSelectedListener)
-        selectedPosition(0)
+        selectedFragment(homeFragment)
     }
 
-    private fun selectedPosition(position: Int) {
-        fragmentTransaction.replace(R.id.home_frame_layout, fragmentList[position])
+    private fun selectedFragment(targetFragment: Fragment) {
+        val fragmentManager: FragmentManager = childFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        if (!targetFragment.isAdded) {
+            fragmentTransaction.hide(currentFragment)
+                .add(R.id.home_frame_layout, targetFragment, targetFragment.javaClass.name)
+                .commit()
+        } else {
+            fragmentTransaction.hide(currentFragment)
+                .show(targetFragment)
+                .commit()
+        }
+        currentFragment = targetFragment
     }
-
 
 }
